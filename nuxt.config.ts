390d98b5f40463de2312176c50e0d7c773585190
a.nikonorov <modeusweb@gmail.com>
2024-10-31 04:18:46 +0300

    base structure

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: false },
+  css: ['@/assets/css/tailwind.css'],
+  buildModules: ['@nuxt/typescript-build'],
 });
